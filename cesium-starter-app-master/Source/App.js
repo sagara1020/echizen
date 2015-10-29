85451223b0907bd5d84c0fefe988594c07267834
sagara1020 <sagara1020@gmail.com>
2015-10-29 15:55:26 +0900

    1

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,11 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+//add a point
+var point = cesiumWidget.entities;
+
+point.add({
+        position : Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+        point : {
+            pixelSize : 10,
+            color : Cesium.Color.YELLOW
+    }
+});
