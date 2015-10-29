9508b0a0b2877e29f326ac7a6580e90d4e8eeef8
sagara1020 <sagara1020@gmail.com>
2015-10-29 16:24:19 +0900

    1

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -9,3 +9,21 @@ point.add({
             color : Cesium.Color.BLUE
     }
 });
+
+var line = cesiumWidget.entities;
+
+line1.add({
+    name : 'Red line on the surface',
+    polyline : {
+        positions : Cesium.Cartesian3.fromDegreesArray([136.137600, 36.218795,
+                                                        136.146183, 36.210070,
+                                                        136.147385, 36.197465,
+                                                        136.155109, 36.191923,
+                                                        136.157513, 36.188183,
+                                                        136.154938, 36.182364,
+                                                        136.146870, 36.175574
+                                                ]),
+        width : 5,
+        material : Cesium.Color.RED
+    }
+});
