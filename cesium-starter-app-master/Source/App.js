8939ab5616cecca7fd311fc3da85a1b467d235da
sagara1020 <sagara1020@gmail.com>
2015-10-29 17:07:56 +0900

    1

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -52,4 +52,4 @@ polygon.add({
 });
 
 cesiumWidget.camera.flyTo({
-    destination : Cesium.Cartesian3.fromDegrees(136.173477, 35.899619, 30000.0)});
+    destination : Cesium.Cartesian3.fromDegrees(136.194763, 36.047711, 40000.0)});
