129c0cd00ad74c55f562f4f1d5a2286af0f57954
sagara1020 <sagara1020@gmail.com>
2015-10-30 16:18:30 +0900

    lasttest

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,5 +1,12 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
 
+Sandcastle.addDefaultToolbarButton('Vehicle', function() {
+    viewer.dataSources.addcesiumWidget.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/echizen/cesium-starter-app-master/Source/refuge.kml'));
+});
+
+Sandcastle.addToolbarButton('Satellites', function() {
+    cesiumWidget.dataSources.add(Cesium.kmlDataSource.load('http://sagara1020.github.io/echizen/cesium-starter-app-master/Source/flood_assumed_area.kml'));
+});
 
 
 //add a point
@@ -56,5 +63,5 @@ polygon.add({
 
 cesiumWidget.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(136.194763, 36.047711, 45000.0)});
 
-cesiumWidget.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/echizen/cesium-starter-app-master/Source/refuge.kml'));
-cesiumWidget.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/echizen/cesium-starter-app-master/Source/flood_assumed_area.kml'));
+//cesiumWidget.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/echizen/cesium-starter-app-master/Source/refuge.kml'));
+//cesiumWidget.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/echizen/cesium-starter-app-master/Source/flood_assumed_area.kml'));
