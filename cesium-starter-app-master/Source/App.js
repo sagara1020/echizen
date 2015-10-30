90ba85d08cd97e8e77a176e17760bca8b8205957
sagara1020 <sagara1020@gmail.com>
2015-10-30 12:01:13 +0900

    kmz

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -53,3 +53,4 @@ polygon.add({
 
 cesiumWidget.camera.flyTo({
     destination : Cesium.Cartesian3.fromDegrees(136.194763, 36.047711, 45000.0)});
+cesiumWidget.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/echizen/cesium-starter-app-master/Source/echizen_map.kmz'));
